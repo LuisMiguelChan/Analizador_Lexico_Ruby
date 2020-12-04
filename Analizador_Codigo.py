b2c99488cf2989fd7bf116a51667e5c851e11d48
LuisChan <60703114+LuisMiguelChan@users.noreply.github.com>
2020-12-04 11:49:14 -0600

    Actualizado

--- Analizador_Codigo.py
+++ Analizador_Codigo.py
@@ -1,4 +1,23 @@
+import ply.lex as lex
+import codecs
+import os
+import sys
 import re
+
+tokens = ['tok','tok']
+
+palreservadas = {
+    '':'',
+    '':''
+}
+
+tokens = tokens + list(palreservadas.values())
+
+t_ignore = ''
+
+t_token = r'expresion reg'
+
+
 dir = input("Nombre del archivo: ")
 f = open(dir, "r")
 while(True):

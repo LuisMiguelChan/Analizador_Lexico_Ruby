f193b5895993092421220a5f474ff26ac7229e40
LuisChan <60703114+LuisMiguelChan@users.noreply.github.com>
2020-12-07 19:33:52 -0600

    Terminado!

--- Ejemplo.rb
+++ Ejemplo.rb
@@ -1,19 +1,9 @@
 x = 33
-y = 3
-sum = x + y 
-res = x - y 
-mult = x * y 
-div = x / y 
-print("La suma de los numeros es ", sum )
-puts("")
-print("La resta de los numeros es ", res )
-puts("")
-print("La multiplicacion de los numeros resulta: ", mult )
-puts("")
-print("La division de los numeros resulta: ", div )
-puts("")
+y = 3.3
 if x < y 
     print( y , " es mayor que ", x )
 else
     print( x ," es mayor que ", y )
 end
+true
+false
--- lexer.py
+++ lexer.py
@@ -1,21 +1,26 @@
 import ply.lex as lex
 
-tokens = ['VARIABLE','EQUALS','SUMAR','RESTAR','MULTIPLICAR','DIVIDIR','NUMBER','COMMA',
-'RPARENT','LPARENT','COMILLA','STRING','MENOR_QUE']
+tokens = ['VARIABLE','SUMAR','RESTAR','MULTIPLICAR','DIVIDIR','NUMBER','COMMA',
+'RPARENT','LPARENT','COMILLA','STRING','MENOR_QUE','MAYOR_QUE','MENOR_IGUAL','MAYOR_IGUAL',
+'DECIMAL','BOLEAN']
 
 palreservadas = {
     'end':'END',
     'print':'PRINT',
     'puts':'PUTS',
     'if':'IF',
-    'else':'ELSE'
+    'else':'ELSE',
+    'and':'AND',
+    'case':'CASE',
+    'def':'DEF',
+    'do':'DO',
+    'when':'WHEN'
 }
 
 tokens = tokens + list(palreservadas.values())
 
 t_ignore = '\t'
 
-t_EQUALS = r'\='
 t_RESTAR = r'\-'
 t_SUMAR = r'\+'
 t_MULTIPLICAR = r'\*'
@@ -25,12 +30,30 @@ t_LPARENT = r'\('
 t_RPARENT = r'\)'
 t_COMILLA = r'\"'
 t_MENOR_QUE = r'<'
+t_MAYOR_QUE = r'>'
+t_MENOR_IGUAL = r'\<\='
+t_MAYOR_IGUAL = r'\>\='
 
 def t_END(t):
     r'end'
     return t
+def t_AND(t):
+    r'and'
+    return t
+def t_CASE(t):
+    r'case'
+    return t
+def t_DEF(t):
+    r'def'
+    return t
+def t_DO(t):
+    r'do'
+    return t
+def t_WHEN(t):
+    r'when'
+    return t
 def t_VARIABLE(t):
-    r'[x][^\w]|[y][^\w]|sum[^\w]|res[^\w]|div[^\w]|mult[^\w]'
+    r'\w+\=|\w+\s\='
     return t
 def t_PRINT(t):
     r'print'
@@ -52,8 +75,15 @@ def t_error(t):
     print("Caracter ilegal '%s'"%t.value[0])
     t.lexer.skip(1)
 def t_NUMBER(t):
-    r'\d+'
+    r'\d+\s'
     t.value = int(t.value)
+    return t
+def t_DECIMAL(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+def t_BOLEAN(t):
+    r'true|false'
     return t
 def t_espacio(t):
     r"\s"
